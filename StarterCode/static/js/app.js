5103795e0c25677703cf5bbf7f2c2a98d8f7744c
smuktar <limodirectphilly@gmail.com>
2021-01-11 17:08:12 -0500

    filter sample values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,18 @@
+// creating a function of Data plotting
 function getPlot(id) {
     // getting data from the json file
     d3.json("samples.json").then((data)=> {
         console.log(data)
     });
+    // using a map function to get Washing Freq and set a variable to hold wfreq
         var wfreq = data.metadata.map(d => d.wfreq)
 
         console.log(`Washing Freq: ${wfreq}`)
+        //filter sample values by id
+        var samples = data.samples.filter(s => s.id.toString()=== id)[0];
+        console.log(samples);
 
-
+        
 }
+
+
