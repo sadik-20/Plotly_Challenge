0bb30d143909729f0aea54ccda6479235bc50c7c
smuktar <limodirectphilly@gmail.com>
2021-01-13 14:47:30 -0500

    get the id data to the dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -147,7 +147,10 @@ function init() {
      // read the data 
      d3.json("samples.json").then((data)=> {
         console.log(data)
-
+    // get the id data to the dropdwown menu
+    data.names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value");
+    });
 
 
 
